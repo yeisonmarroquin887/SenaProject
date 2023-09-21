cc2937eafc13ba9dbecf3d3c90dd84ae937ace01
Yeison Andrés Marroquín Bernal <121591987+yeisonmarroquin887@users.noreply.github.com>
2023-09-20 22:42:44 -0500

    mi project

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+const array = [];
+
 function figuraOnePerimetro() {
     const A = parseFloat(document.getElementById('A').value);
     const B = parseFloat(document.getElementById('B').value);
@@ -62,7 +64,7 @@ function figuraFourPerimetro() {
     const R = parseFloat(document.getElementById("Radio").value)
     const res = 2 * Math.PI * R;
     const result = document.getElementById("PerimetroCirculo")
-     result.textContent = res.toFixed(2)
+    result.textContent = res.toFixed(2)
 }
 figuraFourPerimetro()
 
@@ -70,6 +72,61 @@ function figuraFourArea() {
     const R = parseFloat(document.getElementById("Radio").value)
     const res =  Math.PI * R * R;
     const result = document.getElementById("AreaCirculo")
-     result.textContent = res.toFixed(2)
+    result.textContent = res.toFixed(2)
 }
 figuraFourArea()
+
+//ejercicio 2
+
+function Edades(){
+    const edades = parseFloat(document.getElementById('Ages').value);
+    if (edades >= 1 && edades <= 120) {
+        if (array.length < 10) {
+            array.push(edades);
+        } else {
+            alert("Las 10 edades ya están ingresadas");
+        }
+    } else {
+        alert("Por favor, ingrese una edad válida entre 1 y 120.");
+    }
+
+   
+    let sumaEdades = 0;
+    const menores = [];
+    const mayores = []; 
+    const Abuelos = [];
+    const edadMinima = Math.min(...array)
+    const edadMax = Math.max(...array)
+   for(let i = 0; i < array.length; i++){
+    const ar = array[i]
+         sumaEdades += ar
+    if(ar < 18){
+      menores.push(ar)
+    }else if(ar >= 18){
+       mayores.push(ar)
+     }
+     if(ar >= 60){
+       Abuelos.push(ar)
+     }
+   }
+
+  
+   
+   
+   document.getElementById("formTwo").reset()
+  const cantidad = array.length;
+  const promedio = sumaEdades / cantidad
+  const menoresDeEdad = document.getElementById("menores")
+    menoresDeEdad.textContent = menores.length
+  const mayoresDeEdad = document.getElementById("mayores")
+    mayoresDeEdad.textContent = mayores.length
+  const abuelos = document.getElementById('adultos')
+        abuelos.textContent = Abuelos.length
+  const edadMin = document.getElementById('min')
+      edadMin.textContent = edadMinima
+  const edadmax = document.getElementById('max')
+      edadmax.textContent = edadMax
+  const edadPromedio = document.getElementById('promedio')
+     edadPromedio.textContent = promedio.toFixed(2)
+}
+Edades()
